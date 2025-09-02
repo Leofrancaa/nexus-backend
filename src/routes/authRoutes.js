913e3f3376a04d0e3dc76f76f54d1db8e6453eaf
Leonardo Franca Almeida Silva <leofrancal17@gmail.com>
2025-09-02 12:40:55 -0300

    removing stuff and fixing bugs

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,12 +1,10 @@
 import express from 'express'
-import { registerUser, loginUser, performReset } from '../controllers/authController.js'
+import { registerUser, loginUser } from '../controllers/authController.js'
 
 const router = express.Router()
 
 router.post('/register', registerUser)
 router.post('/login', loginUser)
-router.post('/forgot', forgotPassword);
-router.post('/reset', performReset);
 
 export default router
 
