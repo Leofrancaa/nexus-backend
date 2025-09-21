a689b395667cc25cbb58fc24fde978909851fad9
Leofrancaa <86894430+Leofrancaa@users.noreply.github.com>
2025-09-21 20:44:45 -0300

    v1.0.1 databse test

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
-import { pool, testConnection } from './database/index'
 
 // Routes
 import authRoutes from './routes/authRoutes'
